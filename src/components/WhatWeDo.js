2af4273467c5e6f5f334dbb2641d9967230d2319
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-11 22:34:37 +0100

    add tabs imgs and styling

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -1,10 +1,29 @@
 "use client";
 import { useState } from "react";
 import { Tab } from "@headlessui/react";
+import Link from "next/link";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
+
+const img1 = "/images/what_we_do/1.jpg";
+const img2 = "/images/what_we_do/2.jpg";
+const img3 = "/images/what_we_do/3.jpg";
+const img4 = "/images/what_we_do/4.jpg";
+
+const leadGeneration = [
+  "ABM",
+  "Email Marketing",
+  "Direct Marketing",
+  "Social Media Marketing",
+  "Social Selling",
+  "Sales Promotion",
+];
+
+const listedLeadGeneration = leadGeneration.map((lg) => {
+  return <li>{lg}</li>;
+});
 
 export default function WhatWeDO() {
   let [categories] = useState({
@@ -13,31 +32,35 @@ export default function WhatWeDO() {
         id: 1,
         title: "Leads on Demand: Unleash Your Business Potential",
         text: "Unleash a Flood of Quality Leads with Our Expert Strategies: Harnessing the Power of Email, Direct Marketing, Social Media, and ABM",
-        list: 5,
+        list: listedLeadGeneration,
+        img: img1,
       },
     ],
     Creative: [
       {
-        id: 1,
+        id: 2,
         title: "Is tech making coffee better or worse?",
         text: "Jan 7",
-        list: 29,
+        list: listedLeadGeneration,
+        img: img2,
       },
     ],
     "Content Marketing": [
       {
-        id: 1,
+        id: 3,
         title: "Ask Me Anything: 10 answers to your questions about coffee",
         text: "2d ago",
-        list: 9,
+        list: listedLeadGeneration,
+        img: img3,
       },
     ],
     Events: [
       {
-        id: 1,
+        id: 4,
         title: "Ask Me Anything: 10 answers to your questions about coffee",
         text: "2d ago",
-        list: 9,
+        list: listedLeadGeneration,
+        img: img4,
       },
     ],
   });
@@ -55,9 +78,9 @@ export default function WhatWeDO() {
                 key={category}
                 className={({ selected }) =>
                   classNames(
-                    "w-full py-10 font-playfair font-bold text-2xl first:rounded-tl-xl last:rounded-tr-xl last:shadow-none shadow-[rgba(0,0,15,0.2)_4px_0px_2px_0px]",
+                    "w-full py-10 font-playfair font-bold text-2xl first:rounded-tl-xl last:rounded-tr-xl shadow-[rgba(0,0,15,0.2)_10px_0px_10px_0px]",
                     selected
-                      ? "bg-tab-gray last:shadow-[rgba(0,0,15,0.2)_6px_-2.5px_2px_1px] shadow-[rgba(0,0,15,0.2)_6px_-2.5px_2px_1px] outline-none"
+                      ? "bg-tab-gray shadow-[rgba(0,0,15,0.2)_10px_-10px_10px_0px] outline-none"
                       : " hover:bg-tab-gray/[0.12]"
                   )
                 }
@@ -72,30 +95,38 @@ export default function WhatWeDO() {
                 key={idx}
                 className={classNames("rounded-b-xl bg-tab-gray p-3")}
               >
-                <ul>
+                <div>
                   {posts.map((post) => (
-                    <li
+                    <div
                       key={post.id}
-                      className="relative rounded-md p-3 hover:bg-gray-100"
+                      className="grid grid-cols-2 relative rounded-md p-3"
                     >
-                      <h3 className="text-2xl font-playfair font-bold leading-5">
-                        {post.title}
-                      </h3>
+                      <div className="col-span-2 lg:col-span-1">
+                        <h3 className="text-2xl p-5 font-playfair font-bold">
+                          {post.title}
+                        </h3>
 
-                      <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                        <li>{post.text}</li>
-                        <li>&middot;</li>
-                        <li>{post.list} comments</li>
-                        <li>&middot;</li>
-                      </ul>
-
-                      <a
-                        href="#"
-                        className={classNames("absolute inset-0 rounded-md")}
-                      />
-                    </li>
+                        <div className=" mt-1 px-5 ">
+                          <div className="font-light">{post.text}</div>
+                          <ul className="mt-7 font-light list-disc list-inside">
+                            {post.list}
+                          </ul>
+                          <div className="my-10">
+                            <Link
+                              href="/"
+                              className="bg-s2s-green px-7 py-3 rounded-xl font-playfair font-bold text-white"
+                            >
+                              See examples
+                            </Link>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-span-2 lg:col-span-1 ">
+                        <img src={post.img} alt="illustrations" />
+                      </div>
+                    </div>
                   ))}
-                </ul>
+                </div>
               </Tab.Panel>
             ))}
           </Tab.Panels>
