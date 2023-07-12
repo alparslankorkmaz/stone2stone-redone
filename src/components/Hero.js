10d0f8a31d0101b3a33508b6ac85e8a43200c7e9
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-12 10:08:08 +0100

    fix grid clients sec

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -59,36 +59,46 @@ export default function Hero() {
         <h2 className="font-playfair font-bold text-4xl text-center text-white pt-10">
           Some of our clients
         </h2>
-        <div className="flex justify-around px-14">
-          <img
-            src="/images/clients/cradlepoint.png"
-            alt=""
-            className="w-2/12 aspect-3/2 object-contain p-5"
-          />
+        <div className="grid grid-cols-6 lg:grid-cols-5 justify-center items-center gap-6 lg:gap-3 py-10 lg:py-3 lg:px-14">
+          <div className="col-span-3 lg:col-span-1">
+            <img
+              src="/images/clients/cradlepoint.png"
+              alt=""
+              className="aspect-3/2 object-contain p-3 lg:p-5"
+            />
+          </div>
 
-          <img
-            src="/images/clients/forescout.png"
-            alt=""
-            className="w-2/12 aspect-3/2 object-contain p-5"
-          />
+          <div className="col-span-3 lg:col-span-1">
+            <img
+              src="/images/clients/forescout.png"
+              alt=""
+              className="aspect-3/2 object-contain p-3 lg:p-5"
+            />
+          </div>
 
-          <img
-            src="/images/clients/checkpoint.png"
-            alt=""
-            className="w-2/12 aspect-3/2 object-contain p-5"
-          />
+          <div className="col-span-3 lg:col-span-1">
+            <img
+              src="/images/clients/checkpoint.png"
+              alt=""
+              className="aspect-3/2 object-contain p-3 lg:p-5"
+            />
+          </div>
 
-          <img
-            src="/images/clients/trendmicro.png"
-            alt=""
-            className="w-2/12 aspect-3/2 object-contain p-5"
-          />
+          <div className="col-span-3 lg:col-span-1">
+            <img
+              src="/images/clients/trendmicro.png"
+              alt=""
+              className="aspect-3/2 object-contain p-3 lg:p-5"
+            />
+          </div>
 
-          <img
-            src="/images/clients/cynet.png"
-            alt=""
-            className="w-2/12 aspect-3/2 object-contain p-5"
-          />
+          <div className="col-span-3 lg:col-span-1">
+            <img
+              src="/images/clients/cynet.png"
+              alt=""
+              className="aspect-3/2 object-contain p-3 lg:p-5"
+            />
+          </div>
         </div>
       </div>
     </>
