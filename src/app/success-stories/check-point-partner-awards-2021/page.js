c30162b4393e12f53729fd908beb04a9b129ac91
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-20 17:09:03 +0100

    add check point 21 event page

--- src/app/success-stories/check-point-partner-awards-2021/page.js
+++ src/app/success-stories/check-point-partner-awards-2021/page.js
@@ -1,5 +1,129 @@
+"use client";
 import React from "react";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="bg-[url('/images/stories/page/7.jpg')] bg-cover bg-center bg-no-repeat text-white text-center p-20 lg:p-52">
+        <h1 className="text-2xl lg:text-5xl font-bold font-playfair mb-4">
+          Check Point's Partner Awards 2021
+        </h1>
+        <p className="text-xs lg:text-base font-montserrat font-light">
+          A completely virtual awards ceremony, carefully curated with
+          presenters including special guest Ollie Ollerton, complete with live
+          feed style acceptance speeches and physical awards design, production
+          and delivery.{" "}
+        </p>
+      </div>
+      {/* HERO */}
+      <div className="grid grid-cols-12 py-10 lg:p-16 gap-6 justify-end relative">
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">
+              The Challenge
+            </h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              To create and host the annual Check Point Partner Awards Ceremony,
+              completely virtually. It had to be just as exciting and engaging
+              as its formerly in-person predecessors.{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Target</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Check Point’s partners and employees who were honoured with
+              awards, as well as potential leads.{" "}
+            </p>
+          </div>
+        </div>
+        {/* MIDDLE */}
+        <div className="z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-32 lg:w-96">
+          <div className="relative">
+            <a
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              target="_blank"
+              className="block lg:hidden"
+            >
+              <img src="/images/stories/monitors/7.webp" alt="monitor" />
+            </a>
+            <img
+              src="/images/stories/monitors/empty_monitor.png"
+              alt="monitor"
+              className="hidden lg:block"
+            />
+            <div className="hidden lg:block absolute top-3 left-1/2 -translate-x-1/2">
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                width={360}
+                height={205}
+                controls
+                light
+              />
+            </div>
+          </div>
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Solution</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Through creating virtual backgrounds, social banners and award
+              scripting, Stone2Stone was able to construct all aspects of an in
+              person awards ceremony (minus the celebrity drama).{" "}
+            </p>
+          </div>
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Outcome</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              A gala event with a stunning amount of registrations and hundreds
+              of attendees that created an exciting atmosphere that was engaging
+              for all participants.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* RESULTS */}
+      <div className="lg:px-16 pb-10">
+        <div className="bg-tab-dark-gray rounded-lg flex items-center gap-8 lg:gap-32 p-5 lg:p-10">
+          <h4 className="font-playfair font-bold lg:text-xl">Results</h4>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/reg_icon.png"
+              className="w-4 lg:w-7"
+              alt="registration icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              300 Registrations
+            </p>
+          </div>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/person_icon.png"
+              className="w-4 lg:w-7"
+              alt="person icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              200 Event Attendees
+            </p>
+          </div>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/per_icon.png"
+              className="w-4 lg:w-7"
+              alt="percentage icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              50% Social Media Increase
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
