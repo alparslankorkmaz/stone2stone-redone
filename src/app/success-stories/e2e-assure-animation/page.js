abbc787392c7e47cd6f395b745cd547c3bea9eae
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-20 17:15:34 +0100

    add e2e page

--- src/app/success-stories/e2e-assure-animation/page.js
+++ src/app/success-stories/e2e-assure-animation/page.js
@@ -1,5 +1,123 @@
+"use client";
 import React from "react";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="bg-[url('/images/stories/page/8.jpg')] bg-cover bg-center bg-no-repeat text-white text-center p-20 lg:p-52">
+        <h1 className="text-2xl lg:text-5xl font-bold font-playfair mb-4">
+          e2e Assure Animation
+        </h1>
+        <p className="text-xs lg:text-base font-montserrat font-light">
+          A short, engaging and informative animation showcasing the benefits of
+          e2e assure’s cyber security solutions.{" "}
+        </p>
+      </div>
+      {/* HERO */}
+      <div className="grid grid-cols-12 py-10 lg:p-16 gap-6 justify-end relative">
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">
+              The Challenge
+            </h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              To create an engaging, informative animation explaining the
+              benefits of e2e assure’s SOC that was no more than 90 seconds long
+              and maintained attention throughout.{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Target</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Potential new customers for e2e assure that are unenlightened
+              about the variety of benefits e2e assure’s cyber security
+              solutions provide.{" "}
+            </p>
+          </div>
+        </div>
+        {/* MIDDLE */}
+        <div className="z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-32 lg:w-96">
+          <div className="relative">
+            <a
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              target="_blank"
+              className="block lg:hidden"
+            >
+              <img src="/images/stories/monitors/8.webp" alt="monitor" />
+            </a>
+            <img
+              src="/images/stories/monitors/empty_monitor.png"
+              alt="monitor"
+              className="hidden lg:block"
+            />
+            <div className="hidden lg:block absolute top-3 left-1/2 -translate-x-1/2">
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                width={360}
+                height={205}
+                controls
+                light
+              />
+            </div>
+          </div>
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Solution</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Our team designed, scripted and animated an exciting, clear and
+              concise video animation that promotes the benefits of partnering
+              with e2e assure.{" "}
+            </p>
+          </div>
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Outcome</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Creation of an 80 second animation that was both educational and
+              eye catching, that informed the user of e2e assure’s benefits.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* RESULTS */}
+      <div className="lg:px-16 pb-10">
+        <div className="bg-tab-dark-gray rounded-lg flex items-center gap-8 lg:gap-32 p-5 lg:p-10">
+          <h4 className="font-playfair font-bold lg:text-xl">Results</h4>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/video_icon.png"
+              className="w-4 lg:w-7"
+              alt="video icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">869 Views</p>
+          </div>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/cursor_icon.png"
+              className="w-3 lg:w-5"
+              alt="cursor icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              87 Click Throughs
+            </p>
+          </div>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/share_icon.png"
+              className="w-4 lg:w-7"
+              alt="share icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">32 Shares</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
