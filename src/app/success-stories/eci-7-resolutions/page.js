dcce6917451fda9bf038b42441b9e41ad04a1855
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-20 09:46:05 +0100

    add story page imgs

--- src/app/success-stories/eci-7-resolutions/page.js
+++ src/app/success-stories/eci-7-resolutions/page.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="bg-[url('/images/stories/page/1.jpg')] bg-cover bg-center bg-no-repeat text-white text-center p-20 lg:p-52">
+        <h1 className="text-2xl lg:text-5xl font-bold font-playfair mb-4">
+          ECI 7 Resolutions
+        </h1>
+        <p className="text-xs lg:text-base font-montserrat font-light">
+          A series of videos and corresponding ‘resolutions’ informing viewers
+          about how ECI’s services can transform their business in a visually
+          stimulating environment. New Year, better security!
+        </p>
+      </div>
+    </>
+  );
 }
