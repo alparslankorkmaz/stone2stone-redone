5f7a12f00b9d7b7da9dc47dc1d4ed1ad8a48c5a7
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-20 13:40:45 +0100

    add hero sec to eci page

--- src/app/success-stories/eci-7-resolutions/page.js
+++ src/app/success-stories/eci-7-resolutions/page.js
@@ -13,6 +13,55 @@ export default function page() {
           stimulating environment. New Year, better security!
         </p>
       </div>
+      {/* HERO */}
+      <div className="grid grid-cols-12 py-10 lg:p-16 gap-6 justify-end relative">
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">
+              The Challenge
+            </h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              To create an engaging way to encourage new users to utilise ECI’s
+              services going into 2022.{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Target</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Potential customers whose companies would benefit from ECI’s
+              managed IT, cybersecurity and transformation services.{" "}
+            </p>
+          </div>
+        </div>
+        {/* MIDDLE */}
+        <div className="z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-32 lg:w-96">
+          <img src="/images/stories/monitors/1.webp" alt="" />
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Solution</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              The creation of 7 ‘resolutions’ each demonstrating a benefit of
+              ECI’s services, with a corresponding video hosted by ECI experts
+              engaging in conversation.{" "}
+            </p>
+          </div>
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Outcome</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              A visually engaging series of interesting tech ‘resolutions’
+              backed up by videos hosted by experts from ECI, discussing why
+              each resolution is so beneficial to customers – and why they don’t
+              need to make their own resolutions this year!{" "}
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
