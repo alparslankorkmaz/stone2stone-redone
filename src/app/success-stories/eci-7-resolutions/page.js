ed7c88bad78e5408f5ac553366ef27d3b19d23e2
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-20 14:07:05 +0100

    add result sec to eci page

--- src/app/success-stories/eci-7-resolutions/page.js
+++ src/app/success-stories/eci-7-resolutions/page.js
@@ -38,7 +38,7 @@ export default function page() {
         </div>
         {/* MIDDLE */}
         <div className="z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-32 lg:w-96">
-          <img src="/images/stories/monitors/1.webp" alt="" />
+          <img src="/images/stories/monitors/1.webp" alt="monitor" />
         </div>
         <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
           <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
@@ -58,6 +58,32 @@ export default function page() {
               backed up by videos hosted by experts from ECI, discussing why
               each resolution is so beneficial to customers – and why they don’t
               need to make their own resolutions this year!{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* RESULTS */}
+      <div className="lg:px-16 pb-10">
+        <div className="bg-tab-dark-gray rounded-lg flex items-center gap-8 lg:gap-32 p-5 lg:p-10">
+          <h4 className="font-playfair font-bold lg:text-xl">Results</h4>
+          <div className="flex gap-2 lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/person_icon.png"
+              className="w-4 lg:w-7"
+              alt="person icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              292 Unique Visitors
+            </p>
+          </div>
+          <div className="flex gap-2 lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/video_icon.png"
+              className="w-6 lg:w-11"
+              alt="person icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              481 Video Views
             </p>
           </div>
         </div>
