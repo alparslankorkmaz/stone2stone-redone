7aeec5d276fb9a918537f85bf26d382613a22697
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-21 10:34:05 +0100

    add software one duet and innovation island pages

--- src/app/success-stories/red-hat-innovation-island/page.js
+++ src/app/success-stories/red-hat-innovation-island/page.js
@@ -1,5 +1,104 @@
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="bg-[url('/images/stories/page/15.jpg')] bg-cover bg-center bg-no-repeat text-white text-center p-20 lg:p-52">
+        <h1 className="text-2xl lg:text-5xl font-bold font-playfair mb-4">
+          Red Hat's Innovation Island
+        </h1>
+        <p className="text-xs lg:text-base font-montserrat font-light">
+          Stone2Stone worked with SCC and Red Hat to create Innovation Island. A
+          platform that allowed SCC and Red Hat to market their products and
+          partnership in an innovative way.{" "}
+        </p>
+      </div>
+      {/* HERO */}
+      <div className="grid grid-cols-12 py-10 lg:p-16 gap-6 justify-end relative">
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">
+              The Challenge
+            </h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              To create a central hub for both SCC and Red Hat which would act
+              as a place which clients and targets would go to find out about
+              certain products and services.{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Target</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              New, potential and existing Red Hat and SCC clients. Innovation
+              Island served as an informational centre where anyone could go to
+              do an array of activities.{" "}
+            </p>
+          </div>
+        </div>
+        {/* MIDDLE */}
+        <div className="z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-32 lg:w-96">
+          <img src="/images/stories/monitors/15.webp" alt="monitor" />
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Solution</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              An informational and captivating platform that engaged users with
+              its retro vibe. Allowing users to explore Innovation Island to
+              find information and collect power-ups for the prize draw.{" "}
+            </p>
+          </div>
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Outcome</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              New, potential and existing Red Hat and SCC clients. Innovation
+              Island served as an informational centre where anyone could go to
+              do an array of activities.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* RESULTS */}
+      <div className="lg:px-16 pb-10">
+        <div className="bg-tab-dark-gray rounded-lg flex items-center gap-8 lg:gap-28 p-5 lg:p-10">
+          <h4 className="font-playfair font-bold lg:text-xl">Results</h4>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/person_icon.png"
+              className="w-3 lg:w-5"
+              alt="person icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              200 Users in first week
+            </p>
+          </div>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/controller_icon.png"
+              className="w-4 lg:w-7"
+              alt="controller icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              600+ Playthroughs
+            </p>
+          </div>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/form_icon.png"
+              className="w-4 lg:w-7"
+              alt="form icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              21 Completed Forms
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/success-stories/software-one-duet/page.js
+++ src/app/success-stories/software-one-duet/page.js
@@ -1,5 +1,124 @@
+"use client";
 import React from "react";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="bg-[url('/images/stories/page/14.jpg')] bg-cover bg-center bg-no-repeat text-white text-center p-20 lg:p-52">
+        <h1 className="text-2xl lg:text-5xl font-bold font-playfair mb-4">
+          Software ONE Duet
+        </h1>
+        <p className="text-xs lg:text-base font-montserrat font-light">
+          A dating app style platform with a variety of questions, designed to
+          match users with the cyber security vendor best suited to solve their
+          individual issues.{" "}
+        </p>
+      </div>
+      {/* HERO */}
+      <div className="grid grid-cols-12 py-10 lg:p-16 gap-6 justify-end relative">
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">
+              The Challenge
+            </h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              To create a unique, informative and easy to use platform for
+              SoftwareOne users to find the best vendor partner for them.{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Target</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              SoftwareOne users that are looking for a cyber security solution
+              to their organisation’s issues.{" "}
+            </p>
+          </div>
+        </div>
+        {/* MIDDLE */}
+        <div className="z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-32 lg:w-96">
+          <div className="relative">
+            <a
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              target="_blank"
+              className="block lg:hidden"
+            >
+              <img src="/images/stories/monitors/14.webp" alt="monitor" />
+            </a>
+            <img
+              src="/images/stories/monitors/empty_monitor.png"
+              alt="monitor"
+              className="hidden lg:block"
+            />
+            <div className="hidden lg:block absolute top-3 left-1/2 -translate-x-1/2">
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                width={360}
+                height={205}
+                controls
+                light
+              />
+            </div>
+          </div>
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Solution</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Creation of ‘SoftwareOne Duet’, a dating app style site that
+              easily matches users with the cyber security solution for them.{" "}
+            </p>
+          </div>
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Outcome</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Creation of an interactive landing page, email & social media
+              campaigns all to provide an easy, informative way to solve users’
+              cyber security problems.{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* RESULTS */}
+      <div className="lg:px-16 pb-10">
+        <div className="bg-tab-dark-gray rounded-lg flex items-center gap-8 lg:gap-32 p-5 lg:p-10">
+          <h4 className="font-playfair font-bold lg:text-xl">Results</h4>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/person_icon.png"
+              className="w-3 lg:w-6"
+              alt="person icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">268 Visitors</p>
+          </div>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/journey_icon.png"
+              className="w-4 lg:w-7"
+              alt="journey icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              178 Possible Journeys
+            </p>
+          </div>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/cursor_icon.png"
+              className="w-3 lg:w-5"
+              alt="cursor icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              1240 Click-Throughs
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
