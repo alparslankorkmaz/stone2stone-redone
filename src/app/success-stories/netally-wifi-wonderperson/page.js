dd600148da8f9f8f1feeeb0e710614ed8d0cdcd3
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-21 10:12:09 +0100

    add netally page

--- src/app/success-stories/netally-wifi-wonderperson/page.js
+++ src/app/success-stories/netally-wifi-wonderperson/page.js
@@ -1,5 +1,105 @@
+"use client";
 import React from "react";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="bg-[url('/images/stories/page/11.jpg')] bg-cover bg-center bg-no-repeat text-white text-center p-20 lg:p-52">
+        <h1 className="text-2xl lg:text-5xl font-bold font-playfair mb-4">
+          NetAlly Wifi Wonderperson
+        </h1>
+        <p className="text-xs lg:text-base font-montserrat font-light">
+          Personalised comic book strips were created using the likeness of
+          senior Telco targets to promote the netAlly handheld device, including
+          bespoke posters and landing pages.{" "}
+        </p>
+      </div>
+      {/* HERO */}
+      <div className="grid grid-cols-12 py-10 lg:p-16 gap-6 justify-end relative">
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">
+              The Challenge
+            </h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              To create an utterly unique and engaging campaign for e92plus
+              vendor netAlly’s new handheld device.{" "}
+            </p>
+          </div>
+        </div>
+
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Target</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Senior contacts including engineering and operational directors at
+              Telcos, who’s engineers rely on accurate and reliable testing
+              equipment.{" "}
+            </p>
+          </div>
+        </div>
+        {/* MIDDLE */}
+        <div className="z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-24 lg:w-72">
+          <img src="/images/stories/monitors/11.webp" alt="monitor" />
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Solution</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Creation of ‘Wi-Fi Wonderperson’, our very own superhero with the
+              power to save networks everywhere.{" "}
+            </p>
+          </div>
+        </div>
+        <div className="lg:p-16 col-span-6 bg-tab-dark-gray rounded-lg text-right grid grid-cols-3 items-center">
+          <div className="col-span-3 lg:col-span-2 lg:col-start-2 p-7 lg:p-0">
+            <h3 className="font-playfair font-bold lg:text-xl">The Outcome</h3>
+            <p className="font-montserrat text-xs md:text-sm lg:text-base mt-3">
+              Creation of personalised comic book strips and email campaigns
+              featuring targets as the superhero – because who wouldn’t want to
+              save the (Wi-Fi) world?{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* RESULTS */}
+      <div className="lg:px-16 pb-10">
+        <div className="bg-tab-dark-gray rounded-lg flex items-center gap-8 lg:gap-32 p-5 lg:p-10">
+          <h4 className="font-playfair font-bold lg:text-xl">Results</h4>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/book_icon.png"
+              className="w-4 lg:w-7"
+              alt="book icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              29 Targeted Comic Books
+            </p>
+          </div>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/mail_icon.png"
+              className="w-4 lg:w-7"
+              alt="mail icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              20 Unique Opens
+            </p>
+          </div>
+          <div className="flex gap-2 flex-col lg:flex-row justify-center text-center lg:text-left lg:gap-5 items-center">
+            <img
+              src="/images/stories/icons/cursor_icon.png"
+              className="w-3 lg:w-6"
+              alt="cursor icon"
+            />
+            <p className="font-montserrat text-xs lg:text-base">
+              15 Click-Throughs
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
