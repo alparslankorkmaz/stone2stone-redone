c86713fd85c7647204a8a76f4ea112a0fa0b42b1
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-24 13:10:04 +0100

    fix ourclients padding

--- src/components/OurClients.js
+++ src/components/OurClients.js
@@ -1,16 +1,16 @@
 export default function OurClients() {
   return (
     <>
-      <div className="bg-footer_bg bg-cover bg-center p-8 my-10">
+      <div className="bg-footer_bg bg-cover bg-center p-4 my-10">
         <h2 className="font-playfair font-bold text-4xl text-center text-white pt-10">
           Some of our clients
         </h2>
-        <div className="grid grid-cols-6 lg:grid-cols-5 justify-center items-center gap-6 lg:gap-3 py-10 lg:py-3 lg:px-14">
+        <div className="grid grid-cols-6 lg:grid-cols-5 justify-center items-center gap-3 py-10 lg:py-3 lg:px-24">
           <div className="col-span-3 lg:col-span-1">
             <img
               src="/images/clients/cradlepoint.png"
               alt=""
-              className="aspect-3/2 object-contain p-3 lg:p-5"
+              className="aspect-3/2 object-contain p-8 lg:p-14"
             />
           </div>
 
@@ -18,7 +18,7 @@ export default function OurClients() {
             <img
               src="/images/clients/forescout.png"
               alt=""
-              className="aspect-3/2 object-contain p-3 lg:p-5"
+              className="aspect-3/2 object-contain p-8 lg:p-8"
             />
           </div>
 
@@ -26,7 +26,7 @@ export default function OurClients() {
             <img
               src="/images/clients/checkpoint.png"
               alt=""
-              className="aspect-3/2 object-contain p-3 lg:p-5"
+              className="aspect-3/2 object-contain p-8 lg:p-8"
             />
           </div>
 
@@ -34,7 +34,7 @@ export default function OurClients() {
             <img
               src="/images/clients/trendmicro.png"
               alt=""
-              className="aspect-3/2 object-contain p-3 lg:p-5"
+              className="aspect-3/2 object-contain p-8 lg:p-14"
             />
           </div>
 
@@ -42,7 +42,7 @@ export default function OurClients() {
             <img
               src="/images/clients/cynet.png"
               alt=""
-              className="aspect-3/2 object-contain p-3 lg:p-5"
+              className="aspect-3/2 object-contain p-8 lg:p-14"
             />
           </div>
         </div>
